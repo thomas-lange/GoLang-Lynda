c4294bc61f6cfce2e50d403324fa4b1084f1a108
LeTigre <lotterleben007@gmx.de>
2016-07-03 15:00:28 +0000

    Changing project structure to work with GOPATH

---
+++ src/Structures/Functions.go
@@ -0,0 +1,23 @@
+package main
+
+
+import (
+    "fmt"
+    "foo/bar"
+    )
+    
+    
+
+
+
+
+func main() {
+    operateOnAllIntegers("testFunc", 1, 2, 33, 4)
+    
+    fmt.Println("=========================================")
+    sum, val := SumAll(12, 123, 234 ,2 ,12)
+    
+    fmt.Printf("Sum: %v from %v values!", sum, val)
+    
+    
+}
